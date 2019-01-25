193fcefccd812a4cc3e2556b70ec1b54aec66eaf
Vicheslav Hanchuk <brainon.by@gmail.com>
2019-01-26 01:40:18 +0300

    fixed bugs, added css animation, js functions

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ window.onload = function () {
         if (day < 10) day = "0" + day;
         if (hour < 10) hour = "0" + hour;
         if (min < 10) min = "0" + min;
-        if (sec < 10) sec = "0" + sec;
 
         document.getElementById("date").innerHTML = day + " " + monthArr[month] + " " + year + "г.";
         document.getElementById("time").innerHTML = hour + ":" + min;
@@ -21,10 +20,66 @@ window.onload = function () {
     var timer;
 
     function clockStart() {
-        timer = setInterval(clock, 10000);
+        timer = setInterval(clock, 1000);
         clock();
 
     }
 
     clockStart();
-}
+
+};
+
+$(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            500:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+
+    $('.plusMinus').on('click', function (event) {
+        var plus = $(event.target);
+        if (plus.hasClass('plus')) {
+            plus.removeClass('plus');
+            plus.addClass('minus');
+            var downHill = plus.parent().parent().next();
+            console.log(downHill);
+            // downHill.css('display', 'table-row');
+            downHill.removeAttr('style');
+        } else {
+            plus.removeClass('minus');
+            plus.addClass('plus');
+            var downHill = plus.parent().parent().next();
+            downHill.css('display', 'none');
+        }
+    });
+
+    $('.rangeSum').ionRangeSlider({
+        min: 20,
+        max: 70000,
+        from: 15670,
+        grid: true,
+        postfix: "$",
+    });
+
+    var clock = $('.clock');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 150) {
+            clock.fadeOut();
+        } else {
+            clock.fadeIn();
+        }
+    });
+
+});
